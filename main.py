108c44a2d5b371650b7a96e7c94c6ff232e0dbab
franqong <garciafranconicolas@hotmail.com>
2026-06-11 22:25:15 -0300

    refactor: inicializacion de ventana con root en main y añadidos modelos y controlador

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from vista import main as vistaFuncion
+import tkinter as tk
+from modelo import DiscoveryModel
+from vista import DiscoveryView
+from controlador import DiscoveryController
 
 def main():
-    vistaFuncion()
+    
+    root = tk.Tk() #ahora se inicializa la ventana principal de tkinter de esta forma y no en vista
+    modelo = DiscoveryModel()
+    vista = DiscoveryView(root) #y aca se inicializa el resto del lienzo
+    controlador = DiscoveryController(modelo, vista, root)
+    root.mainloop() #arranca el loop principal
 
 if __name__ == '__main__':
     main()
