6d4235807339d16dded06155417a2eaa54b996fc
franqong <garciafranconicolas@hotmail.com>
2026-06-11 22:23:54 -0300

    feat: callbacks para busqueda

--- controlador.py
+++ controlador.py
@@ -1,5 +1,25 @@
-def main():
-    pass
+import tkinter as tk
+from modelo import DiscoveryModel
+from vista import DiscoveryView
 
-if __name__ == '__main__':
-    main()
+class DiscoveryController:
+    def __init__(self, model: DiscoveryModel, view: DiscoveryView, root: tk.Tk):
+        self.model = model
+        self.view = view
+        self.root = root
+        
+        #enlazar el callback de busqueda de vista
+        self.view.set_callbacks(search_callback=self.on_search)
+
+    def on_search(self, query):
+        print(f"Controlador: Solicitud recibida de búsqueda para: '{query}'")
+        
+        #busca en el modelo
+        resultados = self.model.buscar_cancion(query)
+        
+        if not resultados:
+            print("Controlador: No se encontraron resultados.")
+        else:
+            print(f"Controlador: Se encontraron {len(resultados)} resultado(s):")
+            for idx, song in enumerate(resultados, start=1):
+                print(f"  {idx}. Título: {song['title']} | Artista: {song['artist']['name']} | Álbum: {song['album']['title']}")
